89ade1a7da9a0c19a5fc4bff3c990280814d9174
Gerardo Solis <gerardoa.solis@yahoo.com>
2020-10-13 19:17:58 -0700

    inquirer initializer for options created

--- index.js
+++ index.js
@@ -16,8 +16,63 @@ var connection = mysql.createConnection({
     database: "employee_db"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     console.log("connected!");
-  });
-  
+    start();
+});
+
+function start() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "View All Employees",
+                "View All Employees By Department",
+                "View All Employees By Manager",
+                "Add Employee",
+                "Remove Employee",
+                "Update Employee Role",
+                "Update Employee Manager",
+                "exit"
+
+            ]
+        })
+        .then(function (answer) {
+            switch (answer.action) {
+                case "View All Employees":
+                    viewAllEmployees();
+                    break;
+
+                case "View All Employees By Department":
+                    viewByDepartment();
+                    break;
+
+                case "View All Employees By Manager":
+                    viewByManager();
+                    break;
+
+                case "Add Employee":
+                    addEmployee();
+                    break;
+
+                case "Remove Employee":
+                    removeEmployee();
+                    break;
+
+                case "Update Employee Role":
+                    updateEmpRole();
+                    break;
+
+                case "Update Employee Manager":
+                    updateEmpManager();
+                    break;
+
+                case "exit":
+                    connection.end();
+                    break;
+            }
+        });
+}
