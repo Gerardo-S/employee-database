c7a0669f90e7abbc0be67698378c1c4af48c5557
Gerardo Solis <gerardoa.solis@yahoo.com>
2020-10-13 18:46:41 -0700

    query for all employees made

--- schema.sql
+++ schema.sql
@@ -33,3 +33,20 @@ FOREIGN KEY(manager_id) REFERENCES employees(id)
 );
 
 SELECT * FROM employees;
+-- ====================================================================================================
+-- employee table with manager names column 
+SELECT e.id, e.first_name, e.last_name, e.role_id, CONCAT(m.first_name, " ", m.last_name) AS manager
+FROM employees AS e
+LEFT JOIN employees AS m
+ON e.manager_id = m.id;
+-- ===================================================================================================
+-- employee table with Employee ID |Fisrt Name| Last Name| Manager| Title |Salary
+SELECT e.id, e.first_name, e.last_name, employee_role.title, employee_role.salary, CONCAT(m.first_name, " ", m.last_name) AS manager
+FROM employees AS e LEFT JOIN employees AS m ON e.manager_id = m.id
+LEFT JOIN employee_role ON e.role_id= employee_role.id
+-- ======================================================================================================================================
+-- Taking new table and merging with department to obtain table with All employees 
+-- Employee ID |Fisrt Name| Last Name| Title| Department |Salary| manager
+SELECT e.id, e.first_name, e.last_name, employee_role.title, department.department_name, employee_role.salary, CONCAT(m.first_name, " ", m.last_name) AS manager
+FROM employees AS e LEFT JOIN employees AS m ON e.manager_id = m.id LEFT JOIN employee_role ON e.role_id= employee_role.id
+LEFT JOIN department ON employee_role.department_id= department.id;
